6f26dee91a7c7b562a8cb46e3ff338cd1ff149de
Sampanna <thapaliyasampanna98@gmail.com>
2024-07-10 17:24:21 +1000

    ini_2

--- BFS/Test1.py
+++ BFS/Test1.py
@@ -1,17 +1,18 @@
 import requests
 
 # Configuration
+#HomeAddress of the DVWA
 base_url = "http://127.0.0.1/DVWA/"
 login_url = base_url + "login.php"
 
-# Headers
+# Headers for Mac and Windows OS
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36',
     'Content-Type': 'application/x-www-form-urlencoded'
 }
 
 # List of common passwords to try
-passwords = ["123456", "password", "admin", "admin123", "letmein"]
+passwords = ["123456", "password", "admin", "admin123", "letmein","sydney","corona","ronaldo"]
 
 #define brute force functions
 def brute_force(username):
