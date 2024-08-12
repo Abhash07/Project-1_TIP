8b01883f819772e8405f5270e6ec4852a94ceeb8
Sampanna <thapaliyasampanna98@gmail.com>
2024-08-12 14:08:27 +1000

    sdasd

--- Security_misconfig/set_security.py
+++ Security_misconfig/set_security.py
@@ -6,7 +6,7 @@ def set_dvwa_security(session, base_url, security_level='low'):
     login_data = {
         'username': 'admin',
         'password': 'password',
-        'Login': 'Login'
+        'Login': 'Logi'
     }
     login_url = base_url + 'login.php'
     response = session.get(login_url)
